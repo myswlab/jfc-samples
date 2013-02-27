843956a588f3bf7f50d24c00fa80e44e041a5791
Pietro Martinelli <amicofragile@gmail.com>
2013-02-27 08:13:44 +0000

    Added String.format usage samples

---
+++ src/test/java/it/unibs/ing/fp/strings/StringFormatTest.java
@@ -0,0 +1,27 @@
+package it.unibs.ing.fp.strings;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Locale;
+
+import org.junit.Test;
+
+public class StringFormatTest {
+	private static final String GREETINGS = "Hello";
+	private static final String TARGET = "World";
+
+	@Test
+	public void formatWithStringPlaceholders() throws Exception {
+		final String result = String.format("%s, %s!", GREETINGS, TARGET);
+		assertEquals("Hello, World!", result);
+	}
+	
+	@Test
+	public void formatScientificNotation() throws Exception {
+		final double base = 11.19;
+		final short exp = 5;
+		final String result = String.format(Locale.ENGLISH, "%.3f * 10 ^ %d", base, exp);
+		assertEquals("11.190 * 10 ^ 5", result);
+	}
+}
