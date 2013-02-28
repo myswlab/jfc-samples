28775dc427daa45bdafafb17851baca69c64ccca
Pietro Martinelli <amicofragile@gmail.com>
2013-02-28 08:22:58 +0000

    ENHANCEMENT Samples for Calendar.set, Calendar.add, Calendar.before

--- src/test/java/it/unibs/ing/fp/dates/CalendarTest.java
+++ src/test/java/it/unibs/ing/fp/dates/CalendarTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
 
 public class CalendarTest {
 	private static final Date ORIGIN = new Date(0);
-	
+
 	@Test
 	public void gettingDateFieldsUsingCalendar() throws Exception {
 		Calendar calendar = new GregorianCalendar();
@@ -18,5 +18,25 @@ public class CalendarTest {
 		assertEquals(0, calendar.get(Calendar.MONTH));
 		assertEquals(0, Calendar.JANUARY);
 		assertEquals(1, calendar.get(Calendar.DAY_OF_MONTH));
+	}
+
+	@Test
+	public void settingCalendarFields() throws Exception {
+		Calendar cal1 = createCalendar(1978, Calendar.MARCH, 19);
+		Calendar cal2 = createCalendar(1978, Calendar.MARCH, 23);
+		assertTrue(cal1.before(cal2));
+		cal1.add(Calendar.DAY_OF_MONTH, 4);
+		assertEquals(cal1, cal2);
+		cal1.add(Calendar.DAY_OF_MONTH, 10);
+		assertEquals(Calendar.APRIL, cal1.get(Calendar.MONTH));
+		assertEquals(2, cal1.get(Calendar.DAY_OF_MONTH));
+	}
+
+	private Calendar createCalendar(int year, int month, int day) {
+		Calendar calendar = new GregorianCalendar();
+		calendar.set(Calendar.YEAR, year);
+		calendar.set(Calendar.MONTH, month);
+		calendar.set(Calendar.DAY_OF_MONTH, day);
+		return calendar;
 	}
 }
