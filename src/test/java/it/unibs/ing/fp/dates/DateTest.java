bf426f5c0d2b584e1ee65ee07990b04c46e22e0f
Pietro Martinelli <amicofragile@gmail.com>
2013-03-06 08:05:31 +0000

    ENHANCEMENT Gestting system time in millis and nanos

--- src/test/java/it/unibs/ing/fp/dates/DateTest.java
+++ src/test/java/it/unibs/ing/fp/dates/DateTest.java
@@ -1,5 +1,6 @@
 package it.unibs.ing.fp.dates;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Date;
@@ -14,5 +15,20 @@ public class DateTest {
 		Date second = new Date();
 		assertTrue(first.before(second));
 		assertTrue(second.after(first));
+	}
+	
+	@Test
+	public void gettingCurrentTimeMillis() throws Exception {
+		long first = System.currentTimeMillis();
+		Thread.sleep(10);
+		long second = System.currentTimeMillis();
+		assertTrue(first < second);
+	}
+	
+	@Test
+	public void gettingCurrentTimeNanos() throws Exception {
+		long first = System.nanoTime();
+		long second = System.nanoTime();
+		assertTrue(first < second);
 	}
 }
