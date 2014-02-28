9d1f480c9380202e2460983576fe943f366bdc9b
Pietro Martinelli <amicofragile@gmail.com>
2014-02-28 23:42:20 +0100

    Added DateFormatConverter.convertFirstToSecond and related test

---
+++ src/main/java/it/unibs/ing/fp/converters/DateFormatConverter.java
@@ -0,0 +1,20 @@
+package it.unibs.ing.fp.converters;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateFormatConverter {
+	private final SimpleDateFormat first, second;
+
+	public DateFormatConverter(String first, String second) {
+		this.first = new SimpleDateFormat(first);
+		this.second = new SimpleDateFormat(second);
+	}
+
+	public String convertFirstToSecond(String input) throws ParseException {
+		final Date date = first.parse(input);
+		return second.format(date);
+	}
+
+}
---
+++ src/test/java/it/unibs/ing/fp/converters/DateFormatConverterTest.java
@@ -0,0 +1,17 @@
+package it.unibs.ing.fp.converters;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Implementare una classe DateFormatConverter che, dati due formati di data differenti, esponga metodi di conversione da String che rappresentano una data in un formato a String
+ * che rappresentano la stessa data nell'altro
+ */
+public class DateFormatConverterTest {
+	@Test
+	public void convertFirstFormatToSecond() throws Exception {
+		final DateFormatConverter converter = new DateFormatConverter("dd/MM/yyyy", "yyyyMMdd");
+		assertEquals("20140228", converter.convertFirstToSecond("28/02/2014"));
+	}
+}
