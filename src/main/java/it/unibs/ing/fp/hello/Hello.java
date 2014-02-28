e6d9040e80310c5975a438f0b7439c94247ae005
Pietro Martinelli <amicofragile@gmail.com>
2014-02-28 23:26:22 +0100

    Added support for custom base message

--- src/main/java/it/unibs/ing/fp/hello/Hello.java
+++ src/main/java/it/unibs/ing/fp/hello/Hello.java
@@ -1,13 +1,22 @@
 package it.unibs.ing.fp.hello;
 
 public class Hello {
+	private final String baseMessage;
+
+	public Hello(String baseMessage) {
+		this.baseMessage = baseMessage;
+	}
+
+	public Hello() {
+		this("Hello");
+	}
 
 	public String sayHello() {
 		return sayHelloTo("World");
 	}
 
 	public String sayHelloTo(String target) {
-		return "Hello, " + target + "!";
+		return baseMessage + ", " + target + "!";
 	}
 
 }
--- src/test/java/it/unibs/ing/fp/hello/HelloTest.java
+++ src/test/java/it/unibs/ing/fp/hello/HelloTest.java
@@ -10,12 +10,16 @@ public class HelloTest {
 		final String result = new Hello().sayHello();
 		assertEquals("Hello, World!", result);
 	}
-	
+
 	@Test
 	public void testSayHelloTo() throws Exception {
 		final String result = new Hello().sayHelloTo("boys");
 		assertEquals("Hello, boys!", result);
 	}
-	
-	
+
+	@Test
+	public void testSayHelloWitchCustomMessage() throws Exception {
+		final String result = new Hello("Hi").sayHelloTo("boys");
+		assertEquals("Hi, boys!", result);
+	}
 }
